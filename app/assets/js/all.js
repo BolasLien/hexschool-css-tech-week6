3574f5f2f2200585d7e2bdfa6f356dd0aea293e6
Bolas <ltzuen@gmail.com>
2022-08-28 03:05:45 +0800

    [feat] clean css,made call to action block and add swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+  var swiper = new Swiper(".evaluation-swiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".evaluation-swiper-button-next",
+      prevEl: ".evaluation-swiper-button-prev",
+    },
+    pagination: {
+      el: ".evaluation-swiper-pagination",
+      clickable: true,
+    },
+    loop: true
+  });
