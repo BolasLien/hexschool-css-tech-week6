bbdc535eebca7c432671819ad71c4eb566c11db2
Bolas <ltzuen@gmail.com>
2022-09-01 23:57:36 +0800

    Update 2022-09-01T15:57:33.048Z

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".evaluation-swiper", {
+  spaceBetween: 30,
+  // effect: "fade",
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev"
+  },
+  pagination: {
+    el: ".pagination",
+    bulletActiveClass: "pagination-bullet-active",
+    bulletClass: "pagination-bullet",
+    clickable: true
+  },
+  loop: true
+});
+//# sourceMappingURL=all.js.map
